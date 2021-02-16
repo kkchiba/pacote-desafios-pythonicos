594660c0f4d32f430adc029e215c9229106f64d3
Kleber Chiba <kleber.chiba@gmail.com>
2021-02-16 17:05:55 -0300

    WTTD Primeiros exercicios

--- 01_donuts.py
+++ 01_donuts.py
@@ -11,7 +11,7 @@ e donuts(23) retorna 'Number of donuts: many'
 
 def donuts(count):
     # +++ SUA SOLUÇÃO +++
-    return
+    return 'Number of donuts: %s' % (count if count < 10 else 'many')
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
